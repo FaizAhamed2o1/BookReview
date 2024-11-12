edb8401dacc5e0dadce2f5dbb95022f8a65aa31d
Faiz Ahamed <76490386+FaizAhamed2o1@users.noreply.github.com>
2024-11-12 23:50:57 +0600

    initial project setup done including react-router, fonts and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlack: "#131313",
+        customPrimaryGreen: "#23be0a",
+        customSecondaryBlue: "#59c6d2",
+        customGray08: "#131313cc",
+        customGray07: "#131313b3",
+        customGray06: "#13131399",
+      },
+
+      fontFamily: {
+        heading: ["Playfair Display", "serif"],
+      },
+    },
   },
-  plugins: [],
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
 };
